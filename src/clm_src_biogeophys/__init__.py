3013eb2cae0940fb514414383ebd7a94264bda09
trinav0711 <trinav0711@gmail.com>
2026-02-19 01:30:33 -0500

    Level 2 package cleanup and consolidation

--- src/clm_src_biogeophys/__init__.py
+++
@@ -1,6 +0,0 @@
-"""
-CLM biogeophysics source code.
-
-This module contains Community Land Model (CLM) biogeophysical process implementations,
-including soil temperature, soil state, canopy state, and related modules.
-"""
